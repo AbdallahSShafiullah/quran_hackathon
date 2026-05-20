ac2fee4e2a2000478eade459b5097cd8fc95566f
Abdallah Shafiullah <ashafi@bu.edu>
2026-05-20 17:01:32 -0400

    fix vite deployment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/huda/', // for GitHub Pages — change 'huda' to your repo name
+    plugins: [react()],
+    base: "./"
 })
